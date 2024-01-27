883807f91cb07eac8372f8123fe1ecd377d7619b
Jonathan Kerr <jon.kerr@gmail.com>
2024-01-27 09:28:34 -0800

    Use classes for cleaning to standardize behavior

--- clean_data.py
+++ clean_data.py
@@ -1,27 +1,79 @@
 # data pipeline based on work done for Milestone 1: https://github.com/jonkerr/SIADS593
+import pandas as pd
+import os
+from abc import ABC, abstractmethod
 
-def clean_fred():
-    pass
-
-
-def clean_spy():
-    pass
-
-
-def download(download_option):
-    if download_option in ['fred', 'all']:
-        clean_fred()
-    if download_option in ['spy', 'all']:
-        clean_spy()
+# configure data directory
+RAW_DATA_PATH = './raw_data/'
+CLEAN_DATA_PATH = './clean_data/'
+if not os.path.isdir(CLEAN_DATA_PATH):
+    os.mkdir(CLEAN_DATA_PATH)
 
 
+class EfficientDataCleaner(ABC):
+    def __init__(self, filename) -> None:
+        super().__init__()
+        self.path = CLEAN_DATA_PATH + filename
+
+    def clean_data(self):
+        '''
+        Only clean if the target file doesn't already exist
+        Delete file if exception during clean()
+        '''
+        if os.path.exists(self.path):
+           return      
+        try:  
+            self.clean()
+        except Exception as ex:
+            print('Failed to clean ', self.path)
+            print(ex)
+            # clean up failed clean
+            if os.path.exists(self.path):
+                os.remove(self.path)   
+
+    @abstractmethod
+    def clean(self):
+      pass
+
+
+class CleanSPY(EfficientDataCleaner):
+    '''
+    Download all relevant FRED data and save in CSV format
+    Based on some work in a Python notebook that Naomi created
+    '''
+    def __init__(self, filename='sp500_clean.csv') -> None:
+       super().__init__(filename)
+
+    def clean(self):
+        print('> Cleaning S&P data')
+        sp = pd.read_csv(RAW_DATA_PATH + 'sp500.csv', index_col = 0, parse_dates=True)
+        # resample daily prices to monthly prices
+        sp = sp.resample('MS').first()
+        sp.to_csv(self.path)
+
+
+
+
+
+'''
+Data pipeline based on work done for Milestone 1: https://github.com/jonkerr/SIADS593
+'''
+
+def clean_data(clean_option):
+    if clean_option in ['spy', 'all']:
+        CleanSPY().clean()
+
+
+'''
+Handle command line arguments
+'''
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     # pass an arg using either "-do" or "--download_option"
     parser.add_argument('-co', '--clean_option',
-                        help='Which file to clean? [fred|spy] Default is all',
+                        help='Which file to clean? [spy] Default is all',
                         default="all",
                         required=False)
     args = parser.parse_args()
-    download(args.clean_option)
+    clean_data(args.clean_option)
